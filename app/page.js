fa418d475dbde28896b870dc2c009089f3b4dc04
TDeV4 <117938372+TDeV4@users.noreply.github.com>
2023-07-07 16:43:42 -0500

    Messing with height for logo

--- app/page.js
+++ app/page.js
@@ -14,8 +14,8 @@ export default function Home() {
             <Image
               src="/MCITCentral_Logo2.png"
               alt="MCIT Central Logo"
-              height="80"
-              width="80"
+              height="100"
+              width="100"
               priority
             />
           </a>
