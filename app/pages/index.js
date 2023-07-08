67069012c90dbf14a32dbe97af3b2c575614e596
TDeV4 <117938372+TDeV4@users.noreply.github.com>
2023-07-08 11:36:13 -0500

    New attempt for multiple pages

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from 'app/page.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -39,7 +40,7 @@ export default function Home() {
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/resources"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
