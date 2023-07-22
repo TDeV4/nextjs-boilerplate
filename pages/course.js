1209eada0c37d64207e6e59aad9fc98c9a0a30e2
Alex Quan <alexquan@seas.upenn.edu>
2023-07-22 03:45:09 -0700

    individual course info

---
+++ pages/course.js
@@ -0,0 +1,32 @@
+import IndividualCourseInfo from "@/components/IndividualCourseInfo"
+import styles from 'app/page.module.css'
+import Link from 'next/link'
+import TopNavBar from '../components/TopNavBar';
+import HomeTopBar from '../components/HomeTopBar';
+
+const DUMMY_COURSE_DATA = 
+  { 
+    courseID: '1',
+    courseName: 'Introduction to Software Development',
+    syllabus: null,
+    description: "This course is an introduction to fundamental concepts of programming and computer science.",
+    textbooks: null,
+    summaryReview: null
+  }
+
+  export default function coursePage() {
+    return (
+    <main className={styles.main}>
+        <TopNavBar />
+        <HomeTopBar />
+        <div class={styles.container}>
+        <div class={styles.leftpane}>
+            <div class={styles.borderBox}>
+            <IndividualCourseInfo courseData={DUMMY_COURSE_DATA} />
+            </div>
+        </div>
+        </div>
+    </main>
+    )
+  }
+  
