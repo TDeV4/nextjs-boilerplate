bbf45a61f6f861791702a3f5d596f608d42511d9
Alex Quan <alexquan@seas.upenn.edu>
2023-07-25 07:22:20 -0700

    update individual course page

--- pages/course.js
+++ pages/course.js
@@ -3,8 +3,33 @@ import styles from 'app/page.module.css'
 import Link from 'next/link'
 import TopNavBar from '../components/TopNavBar';
 import HomeTopBar from '../components/HomeTopBar';
-import {getSession} from 'next-auth/react'
+import {getSession} from 'next-auth/react';
+import MyReviewsTab from "@/components/MyReviewsTab";
+import Table from 'react-bootstrap/Table';
 
+const DUMMY_USER_DATA = {
+  userID: "1",
+  name: "Tanner DeVore",
+  anonName: "Purple Panda",
+  timeZone: "CST",
+  expectedGraduation: "Spring 2025",
+  workStatusID: 2,
+  industry: "Cyber-Security",
+  fullTimeStudentStatus: false,
+  inTurtleClub: false,
+  alumniStatus: false,
+  mcitCentralEmailNotifications: true,
+  mcitConnectEmailNotifications: true,
+  commentEmailNotifications: true,
+  MCITConnectEnabled: true,
+  email: "devoret@seas.upenn.edu",
+  linkedinURL: "https://linkedin.com/in/tanner-devore",
+  preferredContactMethod: "slack",
+  bio: "Hello, I am a person who is in the MCIT Program and looking to connect.",
+  education: "Bachelor of Arts in Political Science and Economics",
+  internships: null,
+  marketOutcome: null,
+};
 
 const DUMMY_COURSE_DATA = 
   { 
@@ -46,8 +71,18 @@ const DUMMY_SUP_DATA =
     <main className={styles.main}>
         <TopNavBar />
         <div class={styles.container}>
-        <div class={styles.leftpane}>
+          <div class={styles.leftpane}>
             <div class={styles.borderBox}>
+              <h7 className={styles.centerText}>CIT 591: Introduction to Software Development</h7>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Difficulty:</th>
+                    <th>Workload:</th>
+                    <th>Rating:</th>
+                  </tr>
+                </thead>
+              </Table>
             <IndividualCourseInfo 
             courseData={DUMMY_COURSE_DATA}
             preReqData={DUMMY_PREREQ_DATA}
@@ -55,7 +90,12 @@ const DUMMY_SUP_DATA =
             profData={DUMMY_PROF_DATA}
             semData={DUMMY_SEM_DATA} />
             </div>
-        </div>
+          </div>
+          <div class={styles.rightpane}>
+            <div class={styles.borderBox}>
+              <MyReviewsTab userData={DUMMY_USER_DATA} />
+            </div>
+          </div>
         </div>
     </main>
     )
