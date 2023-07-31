3d8ab90f62be405a4e707e6def00cb629f06b83c
Alex Quan <alexquan@seas.upenn.edu>
2023-07-30 17:04:50 -0700

    added sign in callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,15 @@ import NextAuth from "next-auth/next"
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
+    callbacks: {
+        async signIn({account, profile }) {
+          if (account.provider === "google" && profile.email_verified && profile.email.endsWith("@seas.upenn.com")) {
+            return  Promise.resolve(true)
+            } else {
+                return Promise.resolve(false)
+            }
+          }
+        },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
