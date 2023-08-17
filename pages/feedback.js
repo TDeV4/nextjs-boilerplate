453f2834fb2da0abc1291bf81f055da9abe0a98c
TDeV4 <117938372+TDeV4@users.noreply.github.com>
2023-08-16 23:51:11 -0500

    Added feedback form

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,25 +1,41 @@
-import styles from 'app/page.module.css'
-import Link from 'next/link';
-import {getSession} from 'next-auth/react'
-
+import styles from "app/page.module.css";
+import Link from "next/link";
+import { getSession } from "next-auth/react";
+import TopNavBar from "@/components/TopNavBar";
+import { Button, Form } from "react-bootstrap";
 
 export default function Feedback() {
-    return (
-        <h2>Feedback</h2>
-    )
+  return (
+    <main className={styles.main}>
+      <TopNavBar />
+      <h2>Site Feedback</h2>
+      <p>*Any feedback is welcome and all feedback is anonymous*</p>
+      <Form>
+        <Form.Control
+          as="textarea"
+          size="lg"
+          cols={75}
+          rows={10}
+          placeholder="Feel free to leave us feedback on the site or additional features that you would like to see…
+"
+        />
+        <Button type="submit">Submit</Button>
+      </Form>
+    </main>
+  );
 }
 
 //redirect user to an unaunthenticated page if not authenticated
 export const getServerSideProps = async (context) => {
-    const session = await getSession(context)
-    if (!session) {
-        return {
-            redirect : {    
-                destination : '/login'
-            }
-        }
-    }
+  const session = await getSession(context);
+  if (!session) {
     return {
-        props: {session}
-    }
+      redirect: {
+        destination: "/login",
+      },
+    };
   }
+  return {
+    props: { session },
+  };
+};
