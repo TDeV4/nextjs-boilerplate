ed4dd98978527f0fbea05717a4f62eeca66a9cd3
Aditya Garg <gargkaditya@gmail.com>
2023-08-17 00:42:46 -0700

    Create wrapper with axios to make API calls and auto include headers

---
+++ pages/api/fetchWrapper.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { getSession } from 'next-auth/react';
+
+const ApiClient = () => {
+    const instance = axios.create({
+        baseURL: "http://localhost:8888"
+    });
+  
+    instance.interceptors.request.use(async (request) => {
+      const session = await getSession();
+      if (session) {
+        request.headers.Authorization = `Bearer ${session.idToken}`;
+      }
+      return request;
+    });
+  
+    instance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(`error`, error);
+      },
+    );
+  
+    return instance;
+  };
+  
+  export default ApiClient();
