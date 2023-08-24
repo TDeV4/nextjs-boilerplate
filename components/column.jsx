42c53ca3543c818c6ea4c2cbd60166a386f8d657
TDeV4 <117938372+TDeV4@users.noreply.github.com>
2023-08-24 12:08:13 -0500

    Fixed Column height

--- components/column.jsx
+++ components/column.jsx
@@ -5,32 +5,19 @@ import styles from "../app/page.module.css";
 import CourseSelection from "./CourseSelection";
 import { Droppable } from "react-beautiful-dnd";
 
-const Container = styled.div`
-  flex: 0 0 auto;
-  width: 200px;
-  margin-right: 10px;
-`;
-const Title = styled.h3`
-  padding: 8 px;
-`;
-const CourseList = styled.div``;
-const CourseBubble = styled.div`
-  padding: 5px;
-`;
-
 export default class Column extends React.Component {
   render() {
     return (
       <Col>
         <div className={styles.courseColumnContainer}>
-          <h3>{this.props.column.title}</h3>
+          <h4>{this.props.column.title}</h4>
           <Stack direction="vertical" gap={2}>
             <Droppable droppableId={this.props.column.id.toString()}>
               {(provided) => (
                 <div
                   ref={provided.innerRef}
                   {...provided.droppableProps}
-                  style={{ flexGrow: 1, minHeight: "100px" }}
+                  style={{ minHeight: "100px" }}
                 >
                   {this.props.courses.map((course, index) => (
                     <h2 key={course}>
