06159687a4a884ee0660442c2697aa06a53ab163
Alex Quan <alexquan@seas.upenn.edu>
2023-08-23 21:20:45 -0700

    clean up course pages

--- pages/courses.js
+++ pages/courses.js
@@ -1,41 +1,8 @@
 import styles from 'app/page.module.css'
-import Link from 'next/link';
 import TopNavBar from '../components/TopNavBar';
-import HomeTopBar from '../components/HomeTopBar';
 import {useSession, getSession} from 'next-auth/react';
 import CourseSummary from '@/components/CourseSummary';
 import { useEffect } from "react";
-
-
-
-const DUMMY_COURSE_SUMMARY_DATA = [
-  {
-    courseID: 1,
-    courseNumber: "591",
-    courseName: "Introduction to Software Development",
-    // reviewID: 1,
-    // semester: "Fall 2020",
-    // professor: "Brandon Krakowsky",
-    // // finalGrade: "A",
-    averageDifficulty: 2,
-    averageRating: 4,
-    averageWorkload: 10,
-    reviews: 1,
-  },
-  {
-    courseID: 2,
-    courseNumber: "592",
-    courseName: "Mathematical Foundations of Computer Science",
-    // reviewID: 10,
-    // semester: "Spring 2021",
-    // professor: "Val Tannen",
-    // // finalGrade: null,
-    averageDifficulty: 4,
-    averageRating: 3,
-    averageWorkload: 22,
-    reviews: 2,
-  },
-];
 
 export default function CoursesHome() {
   const { data: session } = useSession();
@@ -51,9 +18,7 @@ export default function CoursesHome() {
         <div class={styles.container}>
         </div>
         <br></br>
-        <CourseSummary
-          courseData={DUMMY_COURSE_SUMMARY_DATA}
-        />
+        <CourseSummary/>
       </main>
     )
 }
