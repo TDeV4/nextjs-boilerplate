220f8bbb85adfb28895796ad7ffd36426a78dd23
Aditya Garg <gargkaditya@gmail.com>
2023-08-24 17:44:26 -0700

    Clean up index.js dummy data

--- pages/index.js
+++ pages/index.js
@@ -11,147 +11,6 @@ import fetchWrapper from "../pages/api/fetchWrapper";
 import CreateProfile from "@/components/CreateProfile";
 import { BrowserRouter } from "react-router-dom";
 
-const DUMMY_USER_DATA = {
-  userID: "1",
-  name: "Tanner DeVore",
-  anonName: "Purple Panda",
-  timeZone: "(GMT-06:00) Central Time",
-  startYear: "2022",
-  graduationYear: "2025",
-  graduationSemester: "Spring",
-  workStatusID: 2,
-  industry: "Cyber-Security",
-  fullTimeStudentStatus: false,
-  inTurtleClub: true,
-  alumniStatus: false,
-  mcitCentralEmailNotifications: true,
-  mcitConnectEmailNotifications: true,
-  commentEmailNotifications: true,
-  MCITConnectEnabled: true,
-  email: "devoret@seas.upenn.edu",
-  linkedinURL: "https://linkedin.com/in/tanner-devore",
-  preferredContactMethod: "slack",
-  bio: "Hello, I am a person who is in the MCIT Program and looking to connect.",
-  education: "Bachelor of Arts in Political Science and Economics",
-  internships: null,
-  marketOutcome: null,
-};
-
-const DUMMY_COURSE_BUILDER_DATA = [
-  {
-    userID: 1,
-    semesterID: 1,
-    courseID: 591,
-  },
-  {
-    userID: 1,
-    semesterID: 1,
-    courseID: 592,
-  },
-  {
-    userID: 1,
-    semesterID: 2,
-    courseID: 593,
-  },
-  {
-    userID: 1,
-    semesterID: 4,
-    courseID: 594,
-  },
-  {
-    userID: 1,
-    semesterID: 5,
-    courseID: 596,
-  },
-  {
-    userID: 1,
-    semesterID: 0,
-    courseID: 595,
-  },
-  {
-    userID: 1,
-    semesterID: 0,
-    courseID: 545,
-  },
-  {
-    userID: 1,
-    semesterID: 0,
-    courseID: 521,
-  },
-  {
-    userID: 1,
-    semesterID: 0,
-    courseID: 551,
-  },
-  {
-    userID: 1,
-    semesterID: 0,
-    courseID: 555,
-  },
-  {
-    userID: 1,
-    semesterID: 5,
-    courseID: 599,
-  },
-];
-
-const DUMMY_COURSES_TAKEN_DATA = [
-  {
-    userID: 1,
-    courseID: 591,
-  },
-  {
-    userID: 1,
-    courseID: 592,
-  },
-  {
-    userID: 1,
-    courseID: 593,
-  },
-  {
-    userID: 1,
-    courseID: 594,
-  },
-  {
-    userID: 1,
-    courseID: 595,
-  },
-  {
-    userID: 1,
-    courseID: 596,
-  },
-];
-
-const DUMMY_CURRENT_COURSES_DATA = [
-  {
-    userID: 1,
-    courseID: 521,
-  },
-];
-
-const DUMMY_ALL_COURSES_DATA = [
-  {
-    courseID: 591,
-    courseName: "Introduction to Software Development",
-  },
-  {
-    courseID: 592,
-    courseName: "Mathematical Foundations of Computer Science",
-  },
-  {
-    courseID: 593,
-    courseName: "Introduction to Computer Systems",
-  },
-  {
-    courseID: 594,
-    courseName: "Data Structures and Software Design",
-  },
-  {
-    courseID: 521,
-    courseName: "Artificial Intelligence",
-  },
-];
-
 export default function HomePage() {
   const { data: session } = useSession();
   //console.log(session);
@@ -202,23 +61,16 @@ export default function HomePage() {
           <div class={styles.container}>
             <div class={styles.leftpane}>
               <div class={styles.borderBox}>
-                <MyProfileTab
-                  session={session}
-                  courseData={DUMMY_COURSES_TAKEN_DATA}
-                  currentCourseData={DUMMY_CURRENT_COURSES_DATA}
-                />
+                <MyProfileTab />
               </div>
               <button onClick={() => signOut()}>Sign out</button>
             </div>
             <div class={styles.rightpane}>
               <div class={styles.borderBox}>
-                <CoursePlanningTab
-                  courseBuilderData={DUMMY_COURSE_BUILDER_DATA}
-                  userData={DUMMY_USER_DATA}
-                />
+                <CoursePlanningTab />
               </div>
               <div class={styles.borderBox}>
-                <MyReviewsTab userData={DUMMY_USER_DATA} />
+                <MyReviewsTab  />
               </div>
             </div>
           </div>
